8156380c0dd67df76a450f0fe7c1bbdd0c4b0c61
Viktor L <viktor.loreth@protonmail.com>
2023-02-02 23:26:43 +0100

    Updated ImageOperations and Masks

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Interpretable AI Resnet50 investigation
+# Interpretable AI 
 
 To use this notebook you need to have the following installed:
 - numpy
@@ -9,10 +9,34 @@ To use this notebook you need to have the following installed:
 Download some images to test saliency maps. I personally used following images:
 - https://github.com/EliSchwartz/imagenet-sample-images
 
-# Next steps:
-1. Model für RESNET50 finden
-2. Basic Analysis of the model
-3. Roar Lesen
-4. Manuell Saliency maps ausprogrammieren
-5. Captum ausprobieren
-6. Vo
+# Papers used in this notebook:
+A Benchmark for Interpretability Methods in Deep Neural Networks (https://arxiv.org/abs/1806.10758)
+Deep Residual Learning for Image Recognition (https://arxiv.org/abs/1512.03385)
+
+# Code used and copied from the following sources:
+Extracting the ImageNetData https://github.com/pytorch/examples/blob/main/imagenet/extract_ILSVRC.sh
+
+
+# Roar Implementation
+
+The implementation varies from the original paper as I am using pytorch and captum instead of tensorflow and saliency.
+
+## Architecture:
+
+The architecture was chosen to be the ResNet18 model because it is easier.
+
+- Model: ResNet18, https://arxiv.org/abs/1512.03385
+- Dataset: ImageNet, http://www.image-net.org/ 
+
+Roar constists of 3 steps:
+
+1. Train a classification model on original dataset. -> Done using the ResNET 18 weights.
+2. Use the trained model to extract attribution maps for each image.
+3. Retrain the model on the original dataset with the attribution maps as additional features. 
+
+## TODO from a code perspective
+
+- Extract the attribution maps for each image. Integrated Gradients, Gradient Saliency, Guide Backpropagation
+- Retrain the model on the original dataset with the attribution maps blanked out as additional features.
+- Calculate the accuracy of the model on the retrained model.
+
---
+++ intereptability_benchmark/saliency_helper_by_vlo.py
@@ -0,0 +1,165 @@
+# coding=utf-8
+# Copyright 2023 Viktor Loreth
+
+"""Saliency helper library to compute and pre-process saliency heatmaps."""
+
+# Integrated Gradients, Gradient Saliency, Guide Backpropagation are used to generate saliency maps
+
+from captum.attr import IntegratedGradients, GuidedBackprop
+from matplotlib.colors import LinearSegmentedColormap
+
+
+def get_saliency_image(model, y, image, saliency_method):
+	"""generates saliency image.
+		Args:
+		  model: model to compute saliency maps.
+		  y: the pre-softmax activation we want to assess attribution with respect to.
+		  image: float32 image tensor with size [1, None, None].
+		  saliency_method: string indicating saliency map type to generate.
+		Returns:
+		  a saliency map and a smoothed saliency map.
+		Raises:
+		  ValueError: if the saliency_method string does not match any included method
+		"""
+	
+	if saliency_method == "integrated_gradients":
+		integrated_placeholder = IntegratedGradients(model)
+		return integrated_placeholder.attribute(image, target=y)
+	
+	elif saliency_method == "guided_backprop":
+		gb_placeholder = GuidedBackprop(model)
+		return gb_placeholder.attribute(image, target=y)
+	
+	else:
+		raise ValueError("No saliency method method matched. Verification of"
+		                 "input needed")
+
+
+def generate_masks( mask):
+	"""
+	:param mask: saliency mask
+	:param threshold: how much of the image should be blacked out (0.5 = 50%)
+	:return: a image with the blocked out saliency mask
+	"""
+	
+	# convert the mask to a 1 channel image
+	mask = mask[0].sum(dim=0).detach().numpy()
+	# create mask 1 with 70% of the pixels blacked out
+	# create mask 2 with 50% of the pixels blacked out
+	# create mask 3 with 30% of the pixels blacked out
+
+	mask1 = mask.copy()
+	mask1 = mask1 > 0.01
+	mask2 = mask.copy()
+	mask2 = mask2 > 0.03
+	mask3 = mask.copy()
+	mask3 = mask3 > 0.05
+
+	print(mask1.sum())
+	print(mask2.sum())
+	print(mask3.sum())
+
+	return mask1, mask2, mask3
+
+
+def apply_mask_to_image(image, mask):
+	# convert True values to ImgNet mean
+	# image = 1 *3*244*244
+	# mask = 244*244
+	# if mask is true, replace the pixel with the mean
+	# if mask is false, keep the pixel
+	# return image
+
+	# apply mask to image
+	image = image[0]
+	image = image.permute(1, 2, 0)
+
+	image[mask] = torch.tensor([0.485, 0.456, 0.406])
+
+	image = image.permute(2, 0, 1)
+	return image
+
+
+if __name__ == '__main__':
+	print("Running saliency Helper by vlo to test the functions")
+	
+	# test the functions
+	import torch
+	import torchvision
+	import torchvision.transforms as transforms
+	import torch.nn.functional as F
+	import json
+	
+	model = torchvision.models.resnet50(weights=torchvision.models.ResNet50_Weights.DEFAULT)
+	model.eval()
+	
+	mean = [0.485, 0.456, 0.406]
+	
+	# Transformer always stays the same
+	transform = transforms.Compose([
+		transforms.Resize(256),
+		transforms.CenterCrop(224),
+		transforms.ToTensor(),
+		transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+	])
+	
+	# load imagenet_labels
+	with open(
+			'C:\\Users\\Vik\\Documents\\4. Private\\01. University\\2022_Sem5\\Intepretable_AI\\datasets\\imagenet_class_index.json') as f:
+		imagenet_labels = json.load(f)
+	imagenet_labels = {int(k): v for k, v in imagenet_labels.items()}
+	
+	from PIL import Image
+	
+	img_path = r'C:\Users\Vik\Documents\4. Private\01. University\2022_Sem5\Intepretable_AI\datasets\imagenet1000samples\n01491361_tiger_shark.JPEG'
+	img = Image.open(img_path)
+	
+	transformed_img = transform(img)
+	input_img = torch.unsqueeze(transformed_img, 0)
+
+	orig_img = input_img.clone()
+	# set model to evaluation mode and run img
+	model.eval()
+	output = model(input_img)
+	output = F.softmax(output, dim=1)
+	prediction_score, pred_label_idx = torch.topk(output, 1)
+	
+	print('Predicted:', pred_label_idx.item(), 'with score:', prediction_score.item())
+	print('Predicted:', imagenet_labels[pred_label_idx.item()])
+	
+	import matplotlib.pyplot as plt
+	
+	# get saliency map
+	saliency_map = get_saliency_image(model, pred_label_idx, input_img, "integrated_gradients")
+
+
+	# plot saliency map
+	plt.imshow(saliency_map[0].permute(1, 2, 0))
+	# plot pixel weight bar
+	plt.colorbar()
+	plt.show()
+
+	# generate mask
+	masks  = generate_masks(saliency_map)
+	
+	# apply all masks to img
+	masked_img1 = apply_mask_to_image(orig_img.clone(), masks[0])
+	masked_img2 = apply_mask_to_image(orig_img.clone(), masks[1])
+	masked_img3 = apply_mask_to_image(orig_img.clone(), masks[2])
+	orig_img = orig_img[0].permute(1, 2, 0)
+	# plot masked img in shape 3*244*244
+	fig, axs = plt.subplots(2, 2)
+	axs[0, 0].imshow(orig_img)
+	axs[0, 0].set_title('Original Image')
+	axs[0, 1].imshow(masked_img1.permute(1, 2, 0))
+	axs[0, 1].set_title('Masked Image 70%')
+	axs[1, 0].imshow(masked_img2.permute(1, 2, 0))
+	axs[1, 0].set_title('Masked Image 50%')
+	axs[1, 1].imshow(masked_img3.permute(1, 2, 0))
+	axs[1, 1].set_title('Masked Image 30%')
+
+
+
+
+
+#%%
