c82cee42094581b82b2f188b998fe20ef0375d91
Viktor L <viktor.loreth@protonmail.com>
2023-04-01 17:07:25 +0200

    Added functional MNIST net evaluation

--- Saliency/saliency_mnist.py
+++ Saliency/saliency_mnist.py
@@ -19,13 +19,13 @@ sys.path.insert(0, '../Training')
 # Windows path:
 path = 'C:\\Users\Vik\Documents\\4. Private\\01. University\\2023_Sem6\\Intepretable_AI\\data\\MNIST\\mod_images'
 
-use_cuda = False # torch.cuda.is_available()
+use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
 torch.cuda.empty_cache()
 torch.cuda.synchronize()
 
-thresholds = [0.3, 0.5, 0.7]
+thresholds = [0.1, 0.5, 0.9]
 
 # load model from ../Training/Train_Mnist.py
 from Training.Train_MNIST import Net
@@ -47,6 +47,12 @@ trainset = torchvision.datasets.MNIST(root='../data', train=True,
 print("Data loaded")
 # generate a folder for each threshold
 if not os.path.exists(path + '\\0.3'):
+	# add a folder for the original images
+	os.mkdir(path + '\\0')
+	# generate a folder for each class
+	for i in range(10):
+		os.mkdir(path + f'\\0\\{i}')
+		
 	for threshold in thresholds:
 		os.mkdir(path + f'\\{threshold}')
 		# generate a folder for each class
@@ -58,10 +64,40 @@ print("Folders created")
 with open(path + '\\labels.txt', 'w') as f:
 	# iterate over all images in the trainset
 	for i in range(len(trainset)):
-		# get the image and the label
+		
+		# get the image an the label
 		img, label = trainset[i]
+		# send img to device
+		img = img.to(device)
+		# save the label in the logfile and end the line
+		f.write(f'{i}, {label} \n')
+		
+		# save the 0 image in folder 0
+		torchvision.utils.save_image(img, path + f'\\0\\{label}\\{i}.png')
+		
 		# calculate the integrated gradients
 		ig = IntegratedGradients(model)
+		# plot the saliency map ig
+		ig_attr = ig.attribute(img.unsqueeze(0), target=label)
+		# flatten ig_attr to 1D
+		ig_attr_flat = ig_attr.view(-1)
 		
+		for ii in range(len(thresholds)):
+			# copy the original image
+			img_masked = img.clone()
+			
+			# find topk indices of most important pixels of ig_attr_flat
+			indices = torch.topk(ig_attr_flat, int(len(ig_attr_flat) * thresholds[ii]))[1]
+			# split up indices into 2D indices
+			indices = torch.stack((indices // 28, indices % 28), dim=1)
+			
+			# set the pixels of the image to mean value of MNIST
+			img_masked[0, indices] = 0.1307
+				
+			# save the image
+			torchvision.utils.save_image(img_masked, path + f'\\{thresholds[ii]}\\{label}\\{i}.png')
+		# track progress
+		if i % 1000 == 0:
+			print(f'{i} images processed')
 		
 print("success")
