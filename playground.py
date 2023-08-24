e18f4bdeba81da5d49481e1150991099d0d2be70
Viktor L <viktor.loreth@protonmail.com>
2023-08-24 15:36:35 +0200

    Update playground.py

--- playground.py
+++ playground.py
@@ -7,20 +7,11 @@ from PIL import Image
 import torch
 import matplotlib.pyplot as plt
 
-path = r'C:\Users\Vik\Documents\4. Private\01. University\2023_Sem6\Intepretable_AI\data\food-101\indices_to_block\integrated_gradient\apple_pie\101251.pt'
+path = '/home/viktorl/Intepretable_AI_PR_Loreth/Data/food-101/indices_to_block/integrated_gradient/tiramisu/1412012.png'
 
-# torch tensor laod from path
-image_array = torch.load(path)
+#load image
+img = Image.open(path)
+#display image on console
+plt.imshow(img)
+plt.show()
 
-print(image_array.shape)
-
-for i in [0,1,2]:
-	print(torch.unique(image_array[i], return_counts=True))
-	
-
-
-
-#plt.imshow(image_array)
-#plt.show()
-
-exit(1)
