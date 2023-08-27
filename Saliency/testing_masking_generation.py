cb42d8f529b9bef1c188d7f22fc8a4b7a9b73ea5
Viktor L <viktor.loreth@protonmail.com>
2023-08-27 12:13:53 +0200

    update

--- Saliency/testing_masking_generation.py
+++ Saliency/testing_masking_generation.py
@@ -17,8 +17,8 @@ import torchvision.transforms as transforms
 
 ####### TESTING MASKING GENERATION: TORCH TENSOR#######
 
-path_mask = r'C:\Users\Vik\Documents\4. Private\01. University\2023_Sem6\Intepretable_AI\data\food-101\indices_to_block\integrated_gradient\apple_pie\134.png'
-img_path= r'C:\Users\Vik\Documents\4. Private\01. University\2023_Sem6\Intepretable_AI\data\food-101\images\apple_pie\134.jpg'
+path_mask = r'C:\Users\Vik\Documents\4. Private\01. University\2023_Sem6\Intepretable_AI\data\food-101\indices_to_block\integrated_gradient\tiramisu\3521611.png'
+img_path= r'C:\Users\Vik\Documents\4. Private\01. University\2023_Sem6\Intepretable_AI\data\food-101\images\tiramisu\3521611.jpg'
 
 
 
@@ -39,15 +39,15 @@ mask = transforms.ToTensor()(mask)
 
 mean_1, mean_2, mean_3 = 0.485, 0.456, 0.406
 
-threshold = 0.5
-# Apply mask to image . the division by 255 is necessary because the mask is saved as a png file and therefore has values between 0 and 255.
-image[0, :, :] = torch.where(mask[0, :, :] < (threshold / 255), image[0, :, :], torch.tensor(mean_1))
-image[1, :, :] = torch.where(mask[1, :, :] < (threshold / 255), image[1, :, :], torch.tensor(mean_2))
-image[2, :, :] = torch.where(mask[2, :, :] < (threshold / 255), image[2, :, :], torch.tensor(mean_3))
-
-# TO control if the mask is applied correctly
-import matplotlib.pyplot as plt
-plt.imshow(image.permute(1, 2, 0))
-plt.show()
+for threshold in [4.5,3.5,2.5,1.5,0.5]:
+	# Apply mask to image . the division by 255 is necessary because the mask is saved as a png file and therefore has values between 0 and 255.
+	image[0, :, :] = torch.where(mask[0, :, :] < (threshold / 255), image[0, :, :], torch.tensor(mean_1))
+	image[1, :, :] = torch.where(mask[1, :, :] < (threshold / 255), image[1, :, :], torch.tensor(mean_2))
+	image[2, :, :] = torch.where(mask[2, :, :] < (threshold / 255), image[2, :, :], torch.tensor(mean_3))
+	
+	# TO control if the mask is applied correctly
+	import matplotlib.pyplot as plt
+	plt.imshow(image.permute(1, 2, 0))
+	plt.show()
 
 
