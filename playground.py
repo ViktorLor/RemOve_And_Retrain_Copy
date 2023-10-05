cd4143cdf7073401e8c051aaf7c2bcf494843339
Viktor L <viktor.loreth@protonmail.com>
2023-10-05 15:17:15 +0200

    Update

--- playground.py
+++ playground.py
@@ -1,30 +1,41 @@
-
-# import libraries
-import os
-import numpy as np
-import PIL
-from PIL import Image
-import torch
+# Import necessary libraries
+from sklearn import tree
 import matplotlib.pyplot as plt
 
-# calaculate std and mean
+# Define the example dataset
+X = [[2.5, 3.0], [1.0, 2.0], [4.0, 2.5], [3.5, 4.0], [5.0, 3.5], [4.5, 1.5]]
+y = [0, 0, 1, 1, 0, 1]
 
-path =r'C:\Users\Vik\Documents\4. Private\01. University\2023_Sem6\Intepretable_AI\models\food101\runs_original\original_ResNet50_lr_0_7_'
 
-list=  []
-for i in range(5):
-	path2 = path + str(i) + '.csv'
-	#load file
-	data = np.genfromtxt(path2, delimiter=',')
-	# only use last row
-	data = data[-1]
-	# only use last value of last row
-	data = data[-1]
-	list.append(data)
 
-# calculate mean and std
-mean = np.mean(list)
-std = np.std(list)
+# Create a decision tree classifier
+clf = tree.DecisionTreeClassifier()
 
-print(mean)
-print(std)
+# Fit the classifier to the dataset
+clf = clf.fit(X, y)
+
+
+
+# Plot the decision tree
+plt.figure(figsize=(9, 6))
+
+# Subplot for the dataset table
+plt.subplot(121)
+plt.axis('off')  # Turn off axis for the table
+plt.title("Dataset")
+# plot X and y
+# join X and y into a single list of lists
+data = [x + [y[i]] for i, x in enumerate(X)]
+#plot the table
+plt.table(cellText=data, colLabels=['Feature 1', 'Feature 2', 'Class'], loc='center')
+
+
+# Subplot for the decision tree
+plt.subplot(122)
+tree.plot_tree(clf, filled=True, feature_names=['Feature 1', 'Feature 2'], class_names=['Class 0', 'Class 1'])
+plt.title("Decision Tree")
+
+plt.tight_layout()
+
+# save plot
+plt.savefig('decision_tree.png')
