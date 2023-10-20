afe566205a4eac7cb59f59cf3d101ac21708db9f
Viktor L <viktor.loreth@protonmail.com>
2023-10-20 15:58:57 +0200

    Updates

--- Reporting/Food101reporting/plot_python.py
+++ Reporting/Food101reporting/plot_python.py
@@ -13,6 +13,12 @@ data = {
 	"Guided_Backprop": {}
 }
 
+data_std_dev = {
+	"Random_Baseline": {},
+	"Integrated_Gradient": {},
+	"Guided_Backprop": {}
+}
+
 fig, ax = plt.subplots(1, 2, figsize=(12, 5))
 width = 0.2
 
@@ -34,15 +40,19 @@ for file_name in file_names:
 			elif ":" in line:
 				key, value = line.split(":")
 				data[current_category][key.strip()] = float(value.split('%')[0].strip())  # Remove '%' symbol
+				if file_name == file_names[1]:
+					data_std_dev[current_category][key.strip()] = float(value.split('-')[1].strip())  # Remove '%' symbol
 
+	# print std dev
+	
 	
 	if file_name == file_names[0]:
 		a = ax[0]
-		a.set_title('Accuracy of the original model')
+		a.set_title('Accuracy of the model in the paper')
 		
 	else:
 		a = ax[1]
-		a.set_title('Accuracy of the modified model')
+		a.set_title('Accuracy of our model')
 		
 	
 	x = np.arange(len(data["Random_Baseline"]))
@@ -50,8 +60,6 @@ for file_name in file_names:
 	a.plot(x, data["Random_Baseline"].values(), label="Random Baseline")
 	a.plot(x, data["Integrated_Gradient"].values(), label="Integrated Gradient")
 	a.plot(x, data["Guided_Backprop"].values(), label="Guided Backprop")
-	a.set_xlabel('Pixels removed in %')
-	a.set_ylabel('Accuracy in %')
 	
 	# set the y axis from 50 to 100
 	a.set_ylim([50, 100])
@@ -59,4 +67,20 @@ for file_name in file_names:
 	a.set_xticks(x)
 	a.set_xticklabels(data["Random_Baseline"].keys())
 	a.legend()
+	# add std dev to plot if second file
+	if file_name == file_names[1]:
+		# convert dict list to dict np array
+		
+		for key in data_std_dev.keys():
+			data_std_dev[key] = np.array([x for x in data_std_dev[key].values()])
+			data[key] = np.array([x for x in data[key].values()])
+		
+		a.fill_between(x, data["Random_Baseline"] - data_std_dev["Random_Baseline"], data["Random_Baseline"] + data_std_dev["Random_Baseline"], alpha=0.2)
+		a.fill_between(x, data["Integrated_Gradient"]- data_std_dev["Integrated_Gradient"], data["Integrated_Gradient"] + data_std_dev["Integrated_Gradient"], alpha=0.2)
+		a.fill_between(x, data["Guided_Backprop"]- data_std_dev["Guided_Backprop"], data["Guided_Backprop"]+ data_std_dev["Guided_Backprop"], alpha=0.2)
+	
+	a.set_xlabel('Pixels removed in %')
+	a.set_ylabel('Accuracy in %')
+	
+
 plt.show()
--- Reporting/MNISTreporting/plotting_report.py
+++ Reporting/MNISTreporting/plotting_report.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 path1 = f"C:/Users/Vik/Documents/4. Private/01. University/2023_Sem6/Intepretable_AI/models/mnist/integrated_gradients/reports"
 path2 = f"C:/Users/Vik/Documents/4. Private/01. University/2023_Sem6/Intepretable_AI/models/mnist/random_baseline/reports"
 # List of file names
@@ -24,30 +24,33 @@ for path in [path1, path2]:
 				random['mean'].append(float(lines[6].split(':')[1].strip()[:-1]))
 				random['std'].append(float(lines[5].split(':')[1].strip()[:-1]))
 		
-
-
+print(ig, random)
 # Plot the mean accuracies in a line plot
 plt.plot(ig['mean'], label='Integrated Gradients')
-# plot the mean accuracies in the same line plot with a different color
+
 plt.plot(random['mean'], label='Random Baseline')
-plt.title('Mean Accuracy vs. Threshold')
-plt.xlabel('Pixels blocked out in %')
+
+# Calculate upper and lower bounds for 'ig' and 'random'
+x = range(len(ig['mean']))
+ig_upper = np.array(ig['mean']) + np.array(ig['std'])
+ig_lower = np.array(ig['mean']) - np.array(ig['std'])
+random_upper = np.array(random['mean']) + np.array(random['std'])
+random_lower = np.array(random['mean']) - np.array(random['std'])
+
+# Fill between the upper and lower bounds for 'ig' and 'random'
+plt.fill_between(x, ig_lower, ig_upper, color='lightblue', alpha=0.5)
+plt.fill_between(x, random_lower, random_upper, color='lightcoral', alpha=0.5)
+plt.title('Accuracy vs Pixels replaced by mean')
+plt.xlabel('Pixels replaced by mean in  %')
 plt.ylabel('Accuracy in %')
 plt.legend()
-plt.xticks(range(len(thresholds)), thresholds)
+
+plt.xticks(range(len(thresholds)), [int(x*100) for x in thresholds])
 
 # save the plot
-path = f"C:/Users/Vik/Documents/4. Private/01. University/2023_Sem6/Intepretable_AI/Latex/figs/"
+path = f"C:/Users/Vik/Documents/4. Private/01. University/2023_Sem6/Intepretable_AI/Latex/PracticalWork/figs/"
 plt.savefig(path + 'mean_accuracy_vs_threshold.png')
 plt.show()
 # plot mean accuracy as
 
-# Plot the std accuracies in a table, with the thresholds as the column names and the std accuracies as the values
-plt.bar(range(len(thresholds)), ig['std'], label='Integrated Gradients')
-plt.bar(range(len(thresholds)), random['std'], label='Random Baseline')
-plt.title('Standard Deviation of Accuracy vs. Threshold')
-plt.xlabel('Pixels blocked out in %')
-plt.ylabel('Standard Deviation of Accuracy in %')
-plt.legend()
-plt.xticks(range(len(thresholds)), thresholds)
-plt.show()
+print(ig['std'], random['std'])
